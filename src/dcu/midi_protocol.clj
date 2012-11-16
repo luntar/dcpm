b5b1e779165bec0f897afc6ddde48febc1de6b3d
John Brinkman <john@brinko.com>
2012-11-16 12:01:47 -0800

    Added DC specific sysex hedaer ref

--- src/dcu/midi_protocol.clj
+++ src/dcu/midi_protocol.clj
@@ -2,13 +2,15 @@
 	(:use [dcu.data-tools])
 )
 
-(defonce DEV-INQ-RESP '(-16 126 0 6 2))
+(defonce DEV-INQ-RESP '(-16 126 0 6 2)) ; per MIDI spec
 
-(def sx-hdr* (ref []))
-(def sx-ver* (ref []))
+(def sx-dc-hdr* (ref []))
+(def sx-man* (ref []))
+(def sx-fam* (ref []))
 (def sx-pid* (ref []))
+(def sx-ver* (ref []))
 
-(defn sx-hdr? [] (not (empty? @sx-hdr*)))
+(defn sx-man? [] (not (empty? @sx-man*)))
 
 (def dnames 
 	(hash-map 
@@ -23,15 +25,19 @@
 (defn clear-sysex-hdr
 	[]
 	(dosync 
-		(ref-set sx-hdr* [])
-		(ref-set sx-pid* [])
+		(ref-set sx-dc-hdr* [])
+		(ref-set sx-man* [])
+		(ref-set sx-fam* [])
+		(ref-set sx-pid* [])	
 		(ref-set sx-ver* [])))
 
 (defn init-sysex
 	[inq-resp]
 	(let [id0 (nth inq-resp 5) 
-	      id1 (nth inq-resp 7) 
-	      id2 (nth inq-resp 6)
+	      id1 (nth inq-resp 6) 
+	      id2 (nth inq-resp 7)
+	      fam0 (nth inq-resp 8)
+	      fam1 (nth inq-resp 9)
 	      pid0 (nth inq-resp 10)
 	      pid1 (nth inq-resp 11)
 	      ver0 (nth inq-resp 12)
@@ -39,8 +45,10 @@
 	      ver2 (nth inq-resp 14)
 	      ver3 (nth inq-resp 15)]
 	(dosync
-		(ref-set sx-hdr* [-16 id0 id1 id2])
-		(ref-set sx-pid* [pid0 pid1])
+		(ref-set sx-dc-hdr* [-16 id0 id1 id2 fam0 pid0])
+		(ref-set sx-man* [-16 id0 id1 id2])
+		(ref-set sx-fam* [fam0 fam1])
+	    (ref-set sx-pid* [pid0 pid1])
 		(ref-set sx-ver* [ver0 ver1 ver2 ver3]))))
 
 (defn init-sysex-manufact
@@ -50,7 +58,7 @@
 			(init-sysex-manufact (nth inq-resp 5) (nth inq-resp 7) (nth inq-resp 6))))
 	([id0 id1 id2]
 	(dosync
-		(ref-set sx-hdr* [-16 id0 id1 id2]))))
+		(ref-set sx-man* [-16 id0 id1 id2]))))
 
 (defn init-sysex-ver
 	"From the given dev-inq responce, or 4 sysex id bytes, initalize the global sysex header"
@@ -76,7 +84,7 @@
 (defn append-sysex-hdr
 	"Append the sysex header to the given vector.  Call init-sysex-manufact before calling this function"
 	[byte-vec]
-	(into @sx-hdr* byte-vec))
+	(into @sx-dc-hdr* byte-vec))
 
 (defn mk-patch-msg
 	"Returns a MIDI byte sequences to retrive the given patch number"
@@ -84,4 +92,4 @@
 	(let [cmd 0x63
 		  hi (get-hi patch-num)
 		  lo (get-lo patch-num)]
-		(append-sysex-hdr [0x12 0x02 cmd hi lo 0xF7])))
+		(append-sysex-hdr [cmd hi lo 0xF7])))
