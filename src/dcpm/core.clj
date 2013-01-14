069d8af6a35d8f5b0ec19c95446d712019159d67
John Brinkman <john@brinko.com>
2013-01-14 07:02:12 -0800

    Added the save preset option.  MIDI port selection is now persistent

--- src/dcpm/core.clj
+++ src/dcpm/core.clj
@@ -3,12 +3,22 @@
   (:use [dcu.midi-protocol])
   (:require dcu.midi-ctrl)
   (:use [dcu.data-tools])
+  (:use [dcu.settings])
   (:use [clojure.tools.cli :only  [cli]])
   (:require  [clojure.string])
   (:use [clojure.pprint])) 
 
-(defn version [] "0.0.1")
+(defn version [] "0.0.2")
 
+(def midi-running* ( atom 0) )
+
+(defn start-midi
+  "starts up the midi subsystem, or if it's running does nothing"
+  []
+  (do (if (zero? @midi-running*)
+        (do (dcu.midi-ctrl/init-midi (dcu.settings/config-get :port)) 
+                                    (dcu.midi-ctrl/id-dev) 
+                                    (swap! midi-running* inc )))))
 (comment 
   (defn t [] 
     (do
@@ -23,12 +33,15 @@
 		(cli args
 			["-h" "--help" "Displays help." :default false :flag true]
 			["-v" "--version" "Prints version." :default false :flag true]
-			["-l" "--list-midi-ports" "Prints MIDI dev descriptions" :default false :flag true]
-			["-r" "--read-patch" "Reads the specific patch number"  :default 0 :parse-fn #(Integer. %)]
+			["-r" "--read-patch" "Reads the specific patch number" :parse-fn #(Integer. %)]
+			["-s" "--save-preset" "Saves the specific preset" :parse-fn #(Integer. %)]
+			["-w" "--write-preset" "Write the specific preset" :parse-fn #(String. %)]
+      ["-d" "--destination" "Destination preset 0-199" :parse-fn #(Integer. %)]
 			["-t" "--run-test" "run specific test number"  :default 0 :parse-fn #(Integer. %)]
       ["-e" "--restore" "Restore specified preset file"]
       ["-c" "--csv" "dump each patch to a csv file"]
-			["-p" "--midi-port" "Use the port with the given 'partial' description" :default "Port"]
+			["-p" "--midi-port" "Sets and saves the MIDI port assigment"]
+			["-l" "--list-midi-ports" "Prints MIDI dev descriptions" :default false :flag true]
 			)]
       
 	   (try
@@ -46,32 +59,58 @@
               (dcu.midi/midi-devices))))
               (System/exit 0))
 
-       ; Start the MIDI system
-       (dcu.midi-ctrl/init-midi (:midi-port options))
-       (dcu.midi-ctrl/id-dev)
+	      (when (:midi-port options)
+              (dcu.settings/config-set! :port (:midi-port options)))
+
+        (println "Using MIDI Port: "  (dcu.settings/config-get :port)) 
+       ;(println "Options: " (count options) " " options)
 
         ( when (not= 0  (:run-test options) )
             ( let [id (:run-test options)]
               (println "run-test")
+              (start-midi)
               (dcu.midi-ctrl/prt-id)
               (System/exit 0)
               ))
 
        (when (:restore options)
          (let [file-name (:restore options)]
+           (println "Restore presets from " file-name)
+           (start-midi)
             (dcu.midi-ctrl/restore-presets file-name))
          (System/exit 0))
 
        (when (:csv options)
          (let [file-name (:csv options)]
+           (println "Dump presets to csvn file " file-name)
+           (start-midi)
             (dcu.midi-ctrl/dump-presets-csv file-name))
          (System/exit 0))
 
        ; Options that require the MIDI susbsystem
        (when (:read-patch options)
          (let [id (:read-patch options)]
+           (println "Read Preset " id)
+           (start-midi)
          (dcu.midi-ctrl/print-patch id))
          (System/exit 0))
+
+       (when (:save-preset options)
+         (let [id (:save-preset options)]
+           (println "Save Preset " id )
+           (start-midi)
+         (dcu.midi-ctrl/preset-save id))
+         (System/exit 0))
+
+       (when (and (:destination options) (:write-preset options))
+         (let [src-fname (:write-preset options)
+               dst-num (:destination options) ]
+           (do
+             (println "Write preset " src-fname " to preset " dst-num  )
+             (start-midi)
+             (load-presets src-fname)
+             (dcu.midi-ctrl/set-preset dst-num)
+             (System/exit 0))))
 
         (catch Exception e
           (println "Internal Error: " e )))
